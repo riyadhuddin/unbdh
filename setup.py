3390312d45fd068469f2148c0b9b8a9bb8cc17ee
Riyadh Uddin <noreplythnx@gmail.com>
2022-11-03 21:20:57 +0600

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='we will be working on youth track',
+    author='iac',
+    license='MIT',
+)
